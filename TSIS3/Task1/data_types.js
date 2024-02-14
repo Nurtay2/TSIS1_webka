c498c5c2dce3ba27000fc2fd2fe987cc08033c5e
Nurzhan Kurmangazy <n_kurmangazy@kbtu.kz>
2024-02-14 14:34:15 +0600

    nurtay

---
+++ TSIS3/Task1/data_types.js
@@ -0,0 +1,7 @@
+console.log(9007199254740991 + 1); // 9007199254740992
+console.log(9007199254740991 + 2); // 9007199254740992
+let n = 12345;
+const bigInt = 1234567890123456789012345678901234567890n;
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
